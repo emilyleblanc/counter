efd6b3bda5cc6ae7e204ffc6d85886eb120102a2
Emily <eleblanc@tvo.org>
2021-09-14 16:54:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import randomColor from "randomcolor";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("")
+
+  useEffect(() => {
+    setColor(randomColor())
+  }, [count])
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{color}}>{count}</h1>
     </div>
   );
 }
